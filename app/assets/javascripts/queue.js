78b9db321e850cb4c9b67281fdfaf0e549aa52b3
Denise <yu.denise.d@gmail.com>
2014-11-27 20:56:03 +0000

    can clear localStorage queue

--- app/assets/javascripts/queue.js
+++ app/assets/javascripts/queue.js
@@ -1,25 +1,25 @@
 jQuery(document).ready(function ($) {
 
-  var queue = document.getElementById('queue')
+  var queue = document.getElementById('queue');
 
   $(queue).bind("DOMSubtreeModified", function() {
-    localStorage.setItem('queue-list', this.innerHTML );
-  })
-
+    localStorage.setItem('queue-list', this.innerHTML);
+  });
 
   $('#results').on('click', '.add-to-queue', function(event){
     event.preventDefault();
     console.log(this);
-    $('#queue').append('<p class="queue-item" id="' + $(this).data('id') + '">' + $(this).data('title')+'</p>')
+    $('#queue').append('<p class="queue-item" id="' + $(this).data('id') + '">' + $(this).data('title')+'</p><br>')
   });
 
   if(localStorage.getItem('queue-list', this.innerHTML)) {
     queue.innerHTML = localStorage.getItem('queue-list');
   };
 
-  // $('#clear-queue').on('click', function(event){
-  //   event.preventDefault();
-  //   window.localStorage.clear();
-  // });
+  $('#clear-queue').on('click', function(event){
+    event.preventDefault();
+    localStorage.clear();
+    queue.innerHTML = localStorage.getItem('queue-list');
+  });
 
 });
