eddaf3662044f6e2290de37468a3eb85303123f8
Denise <yu.denise.d@gmail.com>
2014-12-02 18:01:21 +0000

    Search results horizontal scroll working but search result title must be fixed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -24,7 +24,7 @@ class StaticPagesController < ApplicationController
 	def get_songs
 		q = params[:'search-content'].to_s.gsub(' ', '+') + '+karaoke'
 		@key = ENV['youtube_api_key']
-		@json = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=4&q=#{q}&type=video&order=viewCount&key=#{@key}").parsed_response
+		@json = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&q=#{q}&type=video&order=viewCount&key=#{@key}").parsed_response
 		@track = Track.new
 		@items = @json["items"].collect do |item|
 			HTTParty.get("https://www.googleapis.com/youtube/v3/videos?id=#{item["id"]["videoId"]}&key=#{@key}&part=contentDetails,statistics,snippet").parsed_response
