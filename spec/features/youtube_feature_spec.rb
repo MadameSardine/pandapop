c24c515775939da2f49319cd936de9786ffe55ad
Sam Joseph <tansaku@gmail.com>
2014-12-03 17:32:48 +0000

    don't need instance variables - factory girl?

--- spec/features/youtube_feature_spec.rb
+++ spec/features/youtube_feature_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'youtube searches' do
 
   def preload_playlists
-    @playlist1 = Playlist.create(name: "Makers Jamz")
-    @playlist2 = Playlist.create(name: "Friday Night")
-    @playlist3 = Playlist.create(name: "Katy Perry Jamz")
-    @playlist4 = Playlist.create(name: "Taylor Swift Jamz")
+    Playlist.create(name: "Makers Jamz")
+    Playlist.create(name: "Friday Night")
+    Playlist.create(name: "Katy Perry Jamz")
+    Playlist.create(name: "Taylor Swift Jamz")
   end
 
   before do
