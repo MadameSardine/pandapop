1ec4c8893258f190c0d06a9be7b256323ffb84a2
Denise <yu.denise.d@gmail.com>
2014-12-03 12:11:12 +0000

    Sprinkled some jQuery on the search results bar

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -16,4 +16,33 @@ jQuery(document).ready(function ($) {
     window.location = '/?videoId=' + videoId;
   });
 
+  var position = 0;
+
+  $('#scroll-right-button').on('click', function(event){
+    if (position === -7) {
+      return false
+    } else {
+      position-=1;
+    }
+    console.log(position)
+    event.preventDefault();
+    $('#all-search-results').animate({
+        marginLeft: "-=340px"
+      }, "fast");
+  });
+
+    $('#scroll-left-button').on('click', function(event){
+    if (position === 0) {
+      return false
+    } else {
+      position+=1;
+    }
+      console.log(position)
+      event.preventDefault();
+
+    $('#all-search-results').animate({
+        marginLeft: "+=340px"
+      }, "fast");
+  })
+
 });
