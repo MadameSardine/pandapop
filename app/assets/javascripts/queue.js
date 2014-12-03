04e1b9d72dde32f8eb79c1040457b3ef19ed3093
slstevens <sean.stevens.123@gmail.com>
2014-12-03 20:35:33 +0000

    Add to playlist, correct current track title, delete specific tracks from queue, play specific tracks in queue

--- app/assets/javascripts/queue.js
+++ app/assets/javascripts/queue.js
@@ -33,11 +33,29 @@ jQuery(document).ready(function ($) {
     window.location = '/?videoId='+youtubeVideoId
   });
 
+  $(document).on('click', '.delete-from-queue' , function() {
+    $(this).parents('.queue-item').remove();
+    // localStorage.setItem('queue-list', $queue.html());
+  });
+
+  $('.queue-item-title').on('click', function() {
+    var specificTrackIdInQueue = $(this).parents('.queue-item').data().id;
+    var specificTrackTitleinQueue = $(this).text()
+    $('#player-iframe').attr('src', 'https://youtube.com/embed/' + specificTrackIdInQueue + '?autoplay=1');
+    $('.queue-item').css("background-color", "#5f5f5f");
+    $(this).parents('.queue-item').css("background-color", "gray")
+    $('#current-player-title').text(specificTrackTitleinQueue)
+  });
+
   $('#next-in-queue').on('click', function(event) {
     event.preventDefault();
 
     var secondyoutubeVideoId =  $("#queue article:nth-child(2)").data('id')
+    var secondyoutubeVideoTitle = $("#queue article:nth-child(2)").find('.queue-item-title').text()
+
     $('#player-iframe').attr('src', 'https://youtube.com/embed/' + secondyoutubeVideoId + '?autoplay=1')  
+
+    $('#current-player-title').text(secondyoutubeVideoTitle)
 
     $("#queue article:nth-child(1)").remove()
     localStorage.setItem('queue-list', $queue.html());
